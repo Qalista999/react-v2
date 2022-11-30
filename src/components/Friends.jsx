fc70bd95a2e86380ea323f83f55ba9e15f48f581
Qalista999 <vedmochka-999@yandex.ru>
2022-11-30 11:55:56 +0300

    friends component class

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -1,40 +1,57 @@
+import React from "react";
 import { Link } from "react-router-dom";
 
 const TableRow = (props) => {
-	return (
-		<tr>
-			<th scope="row">{props.index + 1}</th>
-      <td>
-				<Link to={"/profile/" + props.id}>
-				{props.name} {props.lastname}
-				</Link>
-			</td>
-    </tr>
-);
-};
-
-const Friends = (props) => {
-	let users = props.function();
-	let usersCount = Object.keys(users).length;
-	let userRow = [];
-
-	for (let i = 0; i < usersCount; i++) {
-		userRow.push(<TableRow id={users[i].id} key={i} index={i} name={users[i].name} lastname={users[i].lastname} />)
-	}
-
-	return (
-	<table className="table table-bordered">
-  <thead>
+  return (
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">Имя и Фамилия</th>
-      
+      <th scope="row">{props.index + 1}</th>
+      <td>
+        <Link to={"/profile/" + props.id}>
+          {props.name} {props.lastname}
+        </Link>
+      </td>
     </tr>
-  </thead>
-  <tbody>
-      {userRow}
-  </tbody>
-</table>
-	);
+  );
 };
+
+class Friends extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { userRow: [] };
+  }
+
+  componentDidMount() {
+    this.props.function().then((users) => {
+      console.log(users);
+      let usersCount = users.length;
+      let userRow = [];
+
+      for (let i = 0; i < usersCount; i++) {
+        userRow.push(
+          <TableRow
+            id={users[i].id}
+            key={i}
+            index={i}
+            name={users[i].name}
+            lastname={users[i].lastname}
+          />
+        );
+      }
+    });
+  }
+
+  render() {
+    return (
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Имя и Фамилия</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    );
+  }
+}
 export default Friends;
--- src/state.jsx
+++ src/state.jsx
@@ -6,23 +6,26 @@ const user = {
 	about: "Расскажите о себе...",
 	avatar: "https://www.seekpng.com/png/full/356-3562377_personal-user.png",
 }
+let users = {};
+// const users = {
+// 	0: { name: "Валерия", lastname: "Инночкина", id: 4 },
+// 	1: { name: "Пётр", lastname: "Васичкин", id: 9 },
+// 	2: { name: "Павел", lastname: "Васичкин", id: 7 },
+// 	3: { name: "Константин", lastname: "Инночкин", id: 2 },
+// 	4: { name: "Евгения", lastname: "Валентиновна", id: 22 },
+// 	5: { name: "Дмитрий", lastname: "Козлов", id: 17 },
+// }
 
-const users = {
-	0: { name: "Валерия", lastname: "Инночкина", id: 4 },
-	1: { name: "Пётр", lastname: "Васичкин", id: 9 },
-	2: { name: "Павел", lastname: "Васичкин", id: 7 },
-	3: { name: "Константин", lastname: "Инночкин", id: 2 },
-	4: { name: "Евгения", lastname: "Валентиновна", id: 22 },
-	5: { name: "Дмитрий", lastname: "Козлов", id: 17 },
-}
-
-export function getUser(userId) {
-	for (let i = 0; i < Object.keys(users).length; i++) {
-		if (users[i].id == userId) return users[i]; 
-	}
+export function getUser() {
+// 	for (let i = 0; i < Object.keys(users).length; i++) {
+// 		if (users[i].id == userId) return users[i]; 
+// 	}
 	return user;
 }
 
-export function getUsers() {
+export async function getUsers() {
+	let response = await fetch("http://f0740810.xsph.ru/getUsers");
+	users = await response.json();
+	console.log(users);
 	return users;
 }
