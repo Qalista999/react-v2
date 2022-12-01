07e5c0bdeafa1559fcb97785f1a42e8733aeeff7
Qalista999 <vedmochka-999@yandex.ru>
2022-12-01 09:28:49 +0300

    Setting give advice, add shop

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,17 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-function Settings() {
-	const [count, setCount] = useState(0);
-	
-	return <div className="row">
-		<div className="col-6">
-			<p>Количество кликов: {count}</p>
-		</div>
-		<div className="col-6">
-			<button className="btn btn-warning" onClick={() => {
-				setCount(count + 1);
-			}}>Нажми тут</button>
-		</div>
-	</div>;
-}
+const Settings = () => {
+  const [advice, setAdvice] = useState("");
+  useEffect(() => {
+    const url = "https://api.adviceslip.com/advice";
+
+    const fetchData = async () => {
+      try {
+        let response = await fetch(url);
+        let json = await response.json();
+        setAdvice(json.slip.advice);
+      } catch (error) {
+        console.log("error ", error);
+      }
+    };
+    fetchData();
+  }, []);
+  return <div>{advice}</div>;
+};
+
+// function Settings() {
+// 	const [count, setCount] = useState(0);
+
+// 	return <div className="row">
+// 		<div className="col-6">
+// 			<p>Количество кликов: {count}</p>
+// 		</div>
+// 		<div className="col-6">
+// 			<button className="btn btn-warning" onClick={() => {
+// 				setCount(count + 1);
+// 			}}>Нажми тут</button>
+// 		</div>
+// 	</div>;
+// }
 export default Settings;
